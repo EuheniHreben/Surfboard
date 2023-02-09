85f7d3dd169bd96c6a93301e296b003845b79a94
EuheniHreben <tomasblack3@gmail.com>
2023-02-09 19:01:57 +0300

    slider-ready

--- js/slider.js
+++ js/slider.js
@@ -1,1 +1,16 @@
-$('.slider').bxSlider();
+const slider = $('.slider').bxSlider({
+  pager: false,
+  controls:false, 
+  speed: 800,
+  auto: true,
+  pause: 6000
+});
+
+$('.slider-controls__arrow--left').click(e => {
+  e.preventDefault();
+  slider.goToPrevSlide();
+});
+$('.slider-controls__arrow--right').click(e => {
+  e.preventDefault();
+  slider.goToNextSlide();
+});
