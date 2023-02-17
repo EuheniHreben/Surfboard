8eb4bb78cc203b3a366dd33bcf162712ca8f2cd6
EuheniHreben <tomasblack3@gmail.com>
2023-02-17 20:40:51 +0300

    hamburger v2

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,5 +1,5 @@
 const hamburgerMenu = document.getElementById('hamburgerMenu');
-const hamburgerButton = document.getElementById('hamburgerButton');
+const hamburgerButton = document.querySelector('.hamburgerButton');
 const body = document.body;
 
 hamburgerButton.addEventListener('click', e => {
@@ -7,5 +7,13 @@ hamburgerButton.addEventListener('click', e => {
   hamburgerMenu.classList.toggle('active');
   hamburgerButton.classList.toggle('active');
   body.classList.toggle('locked');
+});
 
+hamburgerMenu.addEventListener('click', e => {
+  e.preventDefault();
+  if (e.target.classList.contains('menu__link')) {
+    hamburgerMenu.classList.remove('active');
+    hamburgerButton.classList.remove('active');
+    body.classList.remove('locked');
+  }
 })
--- js/team.js
+++ js/team.js
@@ -1,1 +1,15 @@
 const teamList = document.querySelector('.team__list');
+
+function openItem(button) {
+  const contentWrap = button.nextElementSibling;
+  const content = contentWrap.firstElementChild;
+  const currentHeight = button.offsetHeight;
+}
+
+teamList.addEventListener('click', e => {
+  e.preventDefault();
+  const target = e.target;
+  const activeItem = document.querySelector('.team__title--active');
+  const prevElement = target.previousElementSibling;
+  const parentElement = target.parentElement;
+})
