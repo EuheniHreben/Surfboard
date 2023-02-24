46e9529b34ccfe9d0893fe58fb44f80a79b1daff
EuheniHreben <tomasblack3@gmail.com>
2023-02-25 00:33:58 +0300

    team-js

--- js/team.js
+++ js/team.js
@@ -3,13 +3,39 @@ const teamList = document.querySelector('.team__list');
 function openItem(button) {
   const contentWrap = button.nextElementSibling;
   const content = contentWrap.firstElementChild;
-  const currentHeight = button.offsetHeight;
+  const currentHeight = content.offsetHeight;
+  contentWrap.style.height = currentHeight + 'px';
+  button.classList.add('team__name--active');
+}
+
+function closeItem(button) {
+  if (!button) return;
+  const contentWrap = button.nextElementSibling;
+  contentWrap.style.height = 0;
+  button.classList.remove('team__name--active');
 }
 
 teamList.addEventListener('click', e => {
   e.preventDefault();
   const target = e.target;
-  const activeItem = document.querySelector('.team__title--active');
+  const activeItem = document.querySelector('.team__name--active');
   const prevElement = target.previousElementSibling;
   const parentElement = target.parentElement;
+  if (target.classList.contains('team__name')) {
+    if (target.classList.contains('team__name--active')) {
+      closeItem(target);
+    } else {
+      closeItem(activeItem);
+      openItem(target);
+    }
+  }
+  if (target.classList.contains('team__text')) {
+    closeItem(activeItem);
+  }
+  if (target.classList.contains('team__pos')) {
+    closeItem(activeItem);
+  }
+  if (target.classList.contains('team__content')) {
+    closeItem(activeItem);
+  }
 })
