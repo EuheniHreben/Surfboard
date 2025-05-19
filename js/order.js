03f5ae4ba4a317a6e31056fc07808ef9d5c86cd6
EuheniHreben <tomasblack3@gmail.com>
2025-05-19 21:10:37 +0300

    19.05.25

--- js/order.js
+++ js/order.js
@@ -1,58 +1,28 @@
 const formPage = document.querySelector(".form");
-const sendButtonRu = document.getElementById("sendButtonRu");
-const sendButtonEn = document.getElementById("sendButtonEn");
+const sendButton = document.getElementById("sendButtonRu");
 
-if (sendButtonRu) {
-  sendButtonRu.addEventListener("click", (event) => {
+sendButton.addEventListener("click", (event) => {
+  event.preventDefault();
+  const window = document.createElement("div");
+  window.classList.add("window");
+  formPage.appendChild(window);
+  const message = document.createElement("div");
+  message.classList.add("window__message");
+  window.appendChild(message);
+  const text = document.createElement("div");
+  text.classList.add("window__text");
+  text.textContent = "Сообщение отправлено";
+  message.appendChild(text);
+  const btn = document.createElement("button");
+  btn.classList.add("btn");
+  btn.textContent = "Закрыть";
+  message.appendChild(btn);
+  const body = document.body;
+  body.classList.toggle("locked");
+
+  btn.addEventListener("click", (event) => {
     event.preventDefault();
-    const window = document.createElement("div");
-    window.classList.add("window");
-    formPage.appendChild(window);
-    const message = document.createElement("div");
-    message.classList.add("window__message");
-    window.appendChild(message);
-    const text = document.createElement("div");
-    text.classList.add("window__text");
-    text.innerHTML = "Сообщение отправлено";
-    message.appendChild(text);
-    const btn = document.createElement("div");
-    btn.classList.add("btn");
-    btn.innerHTML = "Закрыть";
-    message.appendChild(btn);
-    const body = document.body;
+    formPage.removeChild(window);
     body.classList.toggle("locked");
-
-    btn.addEventListener("click", (event) => {
-      event.preventDefault();
-      formPage.removeChild(window);
-      body.classList.toggle("locked");
-    });
   });
-} else {
-  sendButtonEn.addEventListener("click", (event) => {
-    event.preventDefault();
-    console.log("hello");
-    const window = document.createElement("div");
-    window.classList.add("window");
-    formPage.appendChild(window);
-    const message = document.createElement("div");
-    message.classList.add("window__message");
-    window.appendChild(message);
-    const text = document.createElement("div");
-    text.classList.add("window__text");
-    text.innerHTML = "Message sent";
-    message.appendChild(text);
-    const btn = document.createElement("div");
-    btn.classList.add("btn");
-    btn.innerHTML = "Close";
-    message.appendChild(btn);
-    const body = document.body;
-    body.classList.toggle("locked");
-
-    btn.addEventListener("click", (event) => {
-      event.preventDefault();
-      formPage.removeChild(window);
-      body.classList.toggle("locked");
-    });
-  });
-}
+});
